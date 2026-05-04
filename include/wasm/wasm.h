102bb562195e524ac24e2f15448ecea717bff63b
Itay Almog <itay2828@gmail.com>
2026-05-04 23:36:23 +0300

    parser: table and element sections

--- include/wasm/wasm.h
+++ include/wasm/wasm.h
@@ -37,6 +37,18 @@ typedef struct wasm_global {
     bool mutable;
 } wasm_global_t;
 
+typedef struct wasm_table {
+    uint32_t min;
+    uint32_t max;
+} wasm_table_t;
+
+typedef struct wasm_elem_segment {
+    uint32_t tableidx;
+    uint32_t offset;
+    uint32_t funcs_count;
+    uint32_t* funcs;
+} wasm_elem_segment_t;
+
 typedef enum wasm_extern_type {
     WASM_EXTERN_FUNC,
     WASM_EXTERN_TABLE,
@@ -79,6 +91,8 @@ typedef struct wasm_module {
     wasm_global_t* globals;
     wasm_import_t* imports;
     wasm_export_t* exports;
+    wasm_table_t* tables;
+    wasm_elem_segment_t* elems;
 
     // same amount as functions count
     wasm_code_t* code;
@@ -91,6 +105,8 @@ typedef struct wasm_module {
     uint32_t globals_count;
     uint32_t imports_count;
     uint32_t exports_count;
+    uint32_t tables_count;
+    uint32_t elems_count;
 } wasm_module_t;
 
 wasm_err_t wasm_load_module(wasm_module_t* module, void* data, size_t size);
