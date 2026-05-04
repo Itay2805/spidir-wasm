dfd1e982b0e4691d944b723480c9a0337ed42545
Itay Almog <itay2828@gmail.com>
2026-05-04 23:07:23 +0300

    util: error/host API headers and basic build glue

---
+++ include/wasm/host.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <spidir/module.h>
+
+typedef enum wasm_host_log_level {
+    WASM_HOST_LOG_RAW,
+    WASM_HOST_LOG_DEBUG,
+    WASM_HOST_LOG_TRACE,
+    WASM_HOST_LOG_WARN,
+    WASM_HOST_LOG_ERROR,
+} wasm_host_log_level_t;
+
+/**
+ * Performs a log on the host, \n is not included
+ */
+void wasm_host_log(wasm_host_log_level_t log_level, const char* fmt, ...);
+
+/**
+ * The size of a single page on the host
+ */
+size_t wasm_host_page_size(void);
+
+void wasm_host_snprintf(char* buffer, size_t len, const char* fmt, ...);
+
+void* wasm_host_calloc(size_t nmemb, size_t size);
+void* wasm_host_realloc(void* ptr, size_t new_size);
+void wasm_host_free(void* ptr);
+
+/**
+ * Allocate a contig region of memory, initially mapped as rw. After the
+ * runtime calls wasm_host_jit_lock, it should turn into rx_page_count and
+ * ro_page_count of rx pages and ro pages respectively.
+ */
+void* wasm_host_jit_alloc(size_t rx_page_count, size_t ro_page_count);
+bool wasm_host_jit_lock(void* ptr, size_t rx_page_count, size_t ro_page_count);
+void wasm_host_jit_free(void* ptr, size_t rx_page_count, size_t ro_page_count);
---
+++ src/util/string.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define memset __builtin_memset
+#define memcpy __builtin_memcpy
+#define memmove __builtin_memmove
+#define strcmp __builtin_strcmp
